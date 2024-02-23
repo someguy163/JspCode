a4bb2cd2ae8ec9ffabb73798b7025b70353ec3c4
someguy163 <wnguddl163@naver.com>
2024-02-23 13:48:16 +0900

    2024-02-23

---
+++ JDBCEx/src/main/java/com/mit/DB/StudentController.java
@@ -0,0 +1,70 @@
+package com.mit.DB;
+
+import java.io.IOException;
+
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.apache.commons.beanutils.BeanUtils;
+
+/**
+ * Servlet implementation class StudentController
+ */
+@WebServlet("/studentControl")
+public class StudentController extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+     StudentDAO dao; 
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public StudentController() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+    public void init(ServletConfig config) throws ServletException { // 프로그램을 자동으로 실행시켜주는 메소드
+    	super.init(config);
+    	dao = new StudentDAO();
+    }
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String action = request.getParameter("action");
+		String view = "";
+		
+		if(action == null) {
+			getServletContext().getRequestDispatcher("/studentControl?action=list")
+			.forward(request, response);
+		} else {
+			switch(action) {
+			case "list" : view = list(request, response);break;
+			case "insert" : view = insert(request, response);break;
+			}
+			getServletContext().getRequestDispatcher("/DB/"+view).forward(request, response);
+		}
+	}
+	
+	public String list(HttpServletRequest request, HttpServletResponse response) {
+		request.setAttribute("students", dao.getAll());
+		return "studentInfo.jsp";
+	}
+	
+	public String insert(HttpServletRequest request, HttpServletResponse response) {
+		Student s = new Student();
+		
+		try {
+		    BeanUtils.populate(s, request.getParameterMap());
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		dao.insert(s);
+		return list(request, response);
+     }
+  }
+
+
--- JDBCEx/src/main/java/com/mit/DB/StudentDAO.java
+++ JDBCEx/src/main/java/com/mit/DB/StudentDAO.java
@@ -3,31 +3,84 @@ package com.mit.DB;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
+//오라클 기반의 데이터베이스 세팅 기본 구문
+// DAO는 DB에 접근해서 데이터를 처리 (CRUD 등)를 위한 객체이다.(데이터베이스에 접근해 데이터를 조회하거나 조작하는 기능을 전담하는 객체)
+//DAO에 메서드로 select, insert, delete, update 와 같이 DB에 접근해 데이터를 조작하는 연산을 구현해놓고, 넣어야하거나 빼내야하는 데이터를 DTO를 사용해서 가져오고 빼낸다.
 public class StudentDAO {
 	Connection conn = null;
+	PreparedStatement pstmt; // SQL 실행을 위한 객체
 
-	PreparedStatement pstmt;
-
-	final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDRiver";
+	final String JDBC_DRIVER = "oracle.jdbc.driver.OracleDriver"; // 오른쪽 주소를 왼쪽 변수에 담기
 	final String JDBC_URL = "jdbc:oracle:thin:@localhost:1521:xe";
 
-	public void open() {
-		try {	
+	public void open() { // 데이터 베이스 접속 구문
+		try {
 			Class.forName(JDBC_DRIVER);
-			conn = DriverManager.getConnection(JDBC_URL,"ora_user","1234");
-
+			conn = DriverManager.getConnection(JDBC_URL, "ora_user", "1234");
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
 	}
 
-	public void close() {
+	public void close() { // 데이터 베이스 해제 구문
 		try {
 			pstmt.close();
 			conn.close();
-		} catch (Exception e) {
-
+		} catch (SQLException e) {
+			e.printStackTrace();
 		}
+	}
+
+	public void insert(Student s) { // insert 메소드
+		open();
+		String sql = "INSERT INTO student(id, username, univ, birth, email) values(SEQ_ID.NEXTVAL,?,?,?,?)"; // sql 변수
+																												// 만들고
+
+		try {
+			pstmt = conn.prepareStatement(sql); // 위 변수를 호출
+			pstmt.setString(1, s.getUsername());
+			pstmt.setString(2, s.getUniv());
+			pstmt.setDate(3, s.getBirth());
+			pstmt.setString(4, s.getEmail());
+
+			pstmt.executeUpdate(); // 수행결과로 Int 타입의 값을 반환합니다.( INSERT / DELETE / UPDATE 관련 구문에서는 반영된 레코드의 건수를
+									// 반환합니다.)
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+	}
+
+	public List<Student> getAll() {
+		open();
+		List<Student> students = new ArrayList<>();
+
+		try {
+			pstmt = conn.prepareStatement("select * from student order by 1");
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				Student s = new Student();
+				s.setId(rs.getInt("id"));
+				s.setUsername(rs.getString("username"));
+				s.setUniv(rs.getString("univ"));
+				s.setBirth(rs.getDate("birth"));
+				s.setEmail(rs.getString("email"));
+
+				students.add(s);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			close();
+		}
+
+		return students;
 	}
 }
